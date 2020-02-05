5bc0bc28d668cf6c62fbfa628151a548242b4add
ddivad195 <ddivad195@gmail.com>
2020-02-05 23:14:54 +0000

    feat(infractions): implement basic infraction flow
    This PR adds the following:
    
    - New dataclasses:
        > GuildMember - respresents the user and will be saved in the `userCollection`. Named `GuildMember` to avoid conflicts with User from the Discord API.
        > Infraction - respresents an Infraction that will be issued to a user.
        > Note - respresents a not that will be added to a user.
    
    - Database Service Updates:
        > Change `insertUserRecord` to `getOrCreateUserRecord` to either retrieve a user from the DB, or create it if it doesn't exist.
        > Implement basic version of the History embed. Update the user's history anytime this is called.
    
    - Infraction conversation updates:
        > Update the infraction conversation to add the steps listed in #1.
        > Currently the infractions are not saved (will come in a later PR).
    
    - Added the `Joda Time` dependency that will be used for Dates and is used in the data classes created above.

--- src/main/kotlin/me/aberrantfox/judgebot/configuration/BotConfiguration.kt
+++ src/main/kotlin/me/aberrantfox/judgebot/configuration/BotConfiguration.kt
@@ -1,31 +1,71 @@
 package me.aberrantfox.judgebot.configuration
 
 import me.aberrantfox.kjdautils.api.annotation.Data
+import org.joda.time.DateTime
 
 @Data("config/config.json")
 data class BotConfiguration(
-    val owner: String = "insert-owner-id",
-    val whitelist: List<String> = listOf("insert-valid-guild-ids"),
-    val guilds: List<GuildConfiguration> = listOf(GuildConfiguration()),
-    val dbConfiguration: DatabaseConfiguration = DatabaseConfiguration()
+        val owner: String = "insert-owner-id",
+        val whitelist: List<String> = listOf("insert-valid-guild-ids"),
+        val guilds: List<GuildConfiguration> = listOf(GuildConfiguration()),
+        val dbConfiguration: DatabaseConfiguration = DatabaseConfiguration()
 )
 
 data class DatabaseConfiguration(
-    val address: String = "localhost:27017"
+        val address: String = "localhost:27017"
 )
 
 data class GuildConfiguration(
-    val id: String = "insert-id",
-    val rules: List<Rule> = listOf(Rule())
+        val id: String = "insert-id",
+        val rules: List<Rule> = listOf(Rule())
 )
 
 data class Rule (
-    val id: String = "Each rule must have a unique, unchanging ID",
-    val title: String = "A short description of the rule",
-    val description: String = "A detailed explanation for clarification purposes",
-    val weight: Int = 1
+        val id: String = "Each rule must have a unique, unchanging ID",
+        val title: String = "A short description of the rule",
+        val description: String = "A detailed explanation for clarification purposes",
+        val weight: Int = 1
 )
+
+data class  Note (
+        val moderator: String,
+        val note: String,
+        val dateTime: DateTime
+)
+
+enum class InfractionWeight {
+    Note, Borderline, Lightly, Clearly, Harshly, Error
+}
+
+data class Infraction(
+        val moderator: String,
+        val reason: String,
+        val dateTime: DateTime,
+        val weight: InfractionWeight
+)
+
+data class GuildMember (
+        val userId: String,
+        val notes: MutableList<Note> = mutableListOf<Note>(),
+        val infractions: MutableList<Infraction> = mutableListOf<Infraction>(),
+        var historyCount: Int = 0,
+        var points: Int = 0
+) {
+    fun incrementHistoryCount() {
+        this.historyCount += 1
+    }
+}
 
 fun GuildConfiguration.isRule(index: Int) = rules.size < index
 
 fun GuildConfiguration.getRuleOrNull(index: Int) = rules.getOrNull(index)
+
+fun convertToInfractionType(infraction: String) =
+        when(infraction.toLowerCase()) {
+            "note" -> InfractionWeight.Note
+            "borderline" -> InfractionWeight.Borderline
+            "lightly" -> InfractionWeight.Lightly
+            "clearly" -> InfractionWeight.Clearly
+            "harshly" -> InfractionWeight.Harshly
+            else -> InfractionWeight.Error
+        }
