55ef8048ae274dc2cd050a457c7c2b76e4c7a516
Steffen Lid Gaustad <579673@stud.hvl.no>
2020-02-06 20:02:08 +0100

    Refactored a range check and changed the rule weight range

--- src/main/kotlin/me/aberrantfox/judgebot/arguments/RuleWeightArg.kt
+++ src/main/kotlin/me/aberrantfox/judgebot/arguments/RuleWeightArg.kt
@@ -6,7 +6,7 @@ import me.aberrantfox.kjdautils.internal.command.ArgumentType
 import me.aberrantfox.kjdautils.internal.command.ConsumptionType
 
 // TODO: Maybe get these in a config?
-const val MAX_WEIGHT: Int = 3
+const val MAX_WEIGHT: Int = 5
 const val MIN_WEIGHT: Int = 1
 
 open class RuleWeightArg(override val name: String = "Weight") : ArgumentType<Int>() {
@@ -18,7 +18,7 @@ open class RuleWeightArg(override val name: String = "Weight") : ArgumentType<In
     override fun convert(arg: String, args: List<String>, event: CommandEvent<*>): ArgumentResult<Int> {
         val weight = arg.toIntOrNull() ?: return ArgumentResult.Error("Expected an integer number, got $arg")
 
-        if (weight < MIN_WEIGHT || weight > MAX_WEIGHT) {
+        if (weight !in MIN_WEIGHT..MAX_WEIGHT) {
             return ArgumentResult.Error("Please input a weight between $MIN_WEIGHT and $MAX_WEIGHT")
         }
         return ArgumentResult.Success(weight)
