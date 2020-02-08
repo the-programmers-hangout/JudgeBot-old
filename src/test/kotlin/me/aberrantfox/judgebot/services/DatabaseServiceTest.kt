bf2cd37b6ec3a2eb31dd78b83f3a1b28a43dcee6
Steffen Lid Gaustad <579673@stud.hvl.no>
2020-02-08 19:55:45 +0100

    Added test stubs for DatabaseService

---
+++ src/test/kotlin/me/aberrantfox/judgebot/services/DatabaseServiceTest.kt
@@ -0,0 +1,32 @@
+package me.aberrantfox.judgebot.services
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class DatabaseServiceTest {
+
+    @Test
+    fun getRule() {
+    }
+
+    @Test
+    fun testGetRule() {
+    }
+
+    @Test
+    fun getRules() {
+    }
+
+    @Test
+    fun addRule() {
+    }
+
+    @Test
+    fun deleteRule() {
+    }
+
+    @Test
+    fun updateRule() {
+    }
+}
