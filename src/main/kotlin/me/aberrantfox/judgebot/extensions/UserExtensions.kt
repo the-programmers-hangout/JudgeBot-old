427701e6d8614d92104c1a0ae59eb10cef4d7b54
ddivad195 <ddivad195@gmail.com>
2020-05-06 23:32:47 +0100

    chore: remove unnecessary file

--- src/main/kotlin/me/aberrantfox/judgebot/extensions/UserExtensions.kt
+++
@@ -1,9 +0,0 @@
-package me.aberrantfox.judgebot.extensions
-
-import me.aberrantfox.kjdautils.extensions.jda.descriptor
-import net.dv8tion.jda.api.entities.User
-import net.dv8tion.jda.api.entities.Role
-
-fun User.verboseDescriptor() = "**${this.asMention} :: ${this.descriptor()}**"
-
-fun Role.verboseDescriptor() = "**${this.asMention} :: ${this.name} :: ${this.id}**"
